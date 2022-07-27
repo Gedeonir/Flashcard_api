b75439bbe99e30b94c7e14f62fb7bb8d52a28bb3
Gedeonir <irafasha.jedy12@gmail.com>
2022-07-27 20:31:56 +0200

    deployment

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import { ApolloServer } from "apollo-server";
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
+
 import { schema } from "./schema";
 import dotenv from 'dotenv'
 import {context} from "./context";
@@ -8,9 +10,11 @@ dotenv.config()
 export const server = new ApolloServer({
     schema,
     context,
+    introspection: true,                                      // 1
+    plugins: [ApolloServerPluginLandingPageLocalDefault()],   // 2
 });
 
-const port = process.env.PORT;
+const port = process.env.PORT||5000;
 
 server.listen({port}).then(({url})=>{
     console.log(`Server is running on ${url}`)
